da06b10255516672d6508db74c4a59d45597fbaf
Jebik <jebik88@gmail.com>
2023-05-26 01:01:03 +0200

    Adding egui

--- src/plugins/state/plugin.rs
+++ src/plugins/state/plugin.rs
@@ -1,4 +1,5 @@
 use bevy::{prelude::{Plugin, App, PluginGroup, default}, window::{WindowPlugin, WindowMode, WindowPosition, Window}, DefaultPlugins};
+use bevy_egui::EguiPlugin;
 use crate::plugins::menu::plugin::MenuPlugin;
 use super::systems::over_state_system;
 // Menu Plugin
@@ -8,15 +9,16 @@ impl Plugin for StatePlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                title: "TileSetEditor For GB ASM".into(),
+                title: "The Veiled Path".into(),
                 position: WindowPosition::At((30,30).into()),
-                resolution: (510., 470.).into(),
+                resolution: (1600., 900.).into(),
                 resizable: false,
                 mode: WindowMode::Windowed,
                 ..default()
             }),
             ..default()
         }));
+        app.add_plugin(EguiPlugin);
         app.add_plugin(MenuPlugin);
         // Add systems, resources, and setup tasks specific to the menu game state
         app.add_system(over_state_system);
