8a74dc192bbe9bb0b7e538bfd4281f1878a3b299
Benjamin LEDRAPPIER <bledrappier@synovo-group.com>
2023-05-26 12:49:13 +0200

    fix menu + ressource usage

--- src/plugins/menu/plugin.rs
+++ src/plugins/menu/plugin.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::{Plugin, App, IntoSystemConfig};
+use bevy::prelude::{Plugin, App, IntoSystemConfig, Resource};
 use bevy::ecs::schedule::OnUpdate;
 use super::systems::menu_ui;
 use crate::plugins::state::types::GameState;
@@ -6,8 +6,17 @@ use crate::plugins::state::types::GameState;
 // Menu Plugin
 pub struct MenuPlugin;
 
+#[derive(Resource)]
+pub enum LevelChoice {
+    None,
+    Tutorial,
+    Level1,
+    Custom,
+}
+
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource::<LevelChoice>(LevelChoice::None);
         app.add_system(menu_ui.in_set(OnUpdate(GameState::Menu)));
     }
 }
--- src/plugins/menu/systems.rs
+++ src/plugins/menu/systems.rs
@@ -1,12 +1,16 @@
-use bevy::{prelude::{info, EventWriter}, app::AppExit};
+use bevy::{prelude::{EventWriter}, app::AppExit};
 use bevy_egui::{ egui::{self}, EguiContexts};
-use crate::plugins::state::types::GameState;
 use bevy::ecs::system::ResMut;
 use bevy::ecs::schedule::NextState;
+use crate::plugins::state::types::GameState;
+use crate::map::map_manager::MapManager;
+use super::plugin::LevelChoice;
 
 pub fn menu_ui(
     mut contexts: EguiContexts,
     mut app_exit_events: EventWriter<AppExit>,
+    map: ResMut<MapManager>,
+    mut level: ResMut<LevelChoice>,
     mut state: ResMut<NextState<GameState>>,
 ) {
     let ctx = contexts.ctx_mut();
@@ -16,20 +20,19 @@ pub fn menu_ui(
             ui.heading("Menu");
 
             if ui.add(egui::Button::new("Tuto").fill(egui::Color32::RED)).clicked() {
-                info!("Tuto clicked");
+                *level = LevelChoice::Tutorial;
                 state.set(GameState::Game);
             }
             if ui.add(egui::Button::new("Level 1").fill(egui::Color32::RED)).clicked() {
-                info!("Level 1 clicked");
+                *level = LevelChoice::Level1;
                 state.set(GameState::Game);
             }
-            /*
-            if let Some(_) = state_manager.map_manager.custom_map {
+            if let Some(_) = map.custom_map {
                 if ui.add(egui::Button::new("Custom").fill(egui::Color32::RED)).clicked() {
-                    info!("Custom clicked");
+                    *level = LevelChoice::Custom;
+                    state.set(GameState::Game);
                 }
             }
-            */
             if ui.add(egui::Button::new("Quit").fill(egui::Color32::RED)).clicked() {
                 app_exit_events.send(AppExit);
             }
