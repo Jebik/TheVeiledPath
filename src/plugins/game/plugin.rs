ca54a89373dd492b5ce63d4f1badf1ff008584a4
Jebik <jebik88@gmail.com>
2023-05-26 18:15:38 +0200

    fix input + add input check

--- src/plugins/game/plugin.rs
+++ src/plugins/game/plugin.rs
@@ -1,7 +1,7 @@
-use bevy::prelude::{Plugin, App, IntoSystemAppConfig};
-use bevy::ecs::schedule::OnEnter;
+use bevy::prelude::{Plugin, App, IntoSystemAppConfig, IntoSystemConfig};
+use bevy::ecs::schedule::{OnEnter,OnUpdate};
 use super::systems::setup_game;
-use super::input::setup_input;
+use super::input::{setup_input, move_system};
 use crate::plugins::state::types::GameState;
 
 // Game Plugin
@@ -12,5 +12,6 @@ impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(setup_input);
         app.add_system(setup_game.in_schedule(OnEnter(GameState::Game)));
+        app.add_system(move_system.in_set(OnUpdate(GameState::Game)));
     }    
 }
