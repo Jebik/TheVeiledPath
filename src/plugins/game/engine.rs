cf2507d7ced8c4008538256f79d03b600ef0d927
Jebik <jebik88@gmail.com>
2023-05-26 18:18:27 +0200

    Fix player

--- src/plugins/game/engine.rs
+++ src/plugins/game/engine.rs
@@ -6,12 +6,16 @@ use crate::map::json_types::{MapData, Dimension};
 pub struct Player {
     pub x: f32,
     pub y: f32,
+    pub goal_x: i32,
+    pub goal_y: i32,
 }
 impl Player {
     fn new(level_data: &MapData) -> Player {
         Player { 
             x: level_data.start_x as f32 + 0.5,
-            y: level_data.start_y as f32 + 0.5
+            y: level_data.start_y as f32 + 0.5,
+            goal_x: level_data.goal_x,
+            goal_y: level_data.goal_y
         }
     }
 }
