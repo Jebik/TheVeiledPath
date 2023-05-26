f824543dd223cd4b84fa3be969d5ecb0cd70b439
Benjamin LEDRAPPIER <bledrappier@synovo-group.com>
2023-05-26 13:07:57 +0200

    map selection

--- src/map/map_manager.rs
+++ src/map/map_manager.rs
@@ -6,16 +6,16 @@ use crate::map::{
 };
 
 pub struct MapManager {
-    _tuto_map: Map,
-    _level1_map: Map,
+    pub tuto_map: Map,
+    pub level1_map: Map,
     pub custom_map: Option<Map>,
 }
 
 impl MapManager {
     pub fn new(tuto_map: MapData, level_map: MapData) -> Result<Self, Box<dyn Error>> {
         Ok(Self {
-            _tuto_map: generate_map(tuto_map)?,
-            _level1_map: generate_map(level_map)?,
+            tuto_map: generate_map(tuto_map)?,
+            level1_map: generate_map(level_map)?,
             custom_map: None,
         })
     }
--- src/plugins/game/systems.rs
+++ src/plugins/game/systems.rs
@@ -1,7 +1,25 @@
 use super::plugin::GameData;
 use bevy::ecs::system::Commands;
+use crate::plugins::menu::plugin::LevelChoice;
+use crate::map::map_manager::MapManager;
+use bevy::ecs::system::Res;
 
-pub fn setup_game(mut commands: Commands) {
+pub fn setup_game(
+    mut commands: Commands,
+    level: Res<LevelChoice>,
+    map: Res<MapManager>,
+) {
+    let level_data = match *level {
+        LevelChoice::Tutorial => &map.tuto_map,
+        LevelChoice::Level1 => &map.level1_map,
+        LevelChoice::Custom => match &map.custom_map {
+            Some(custom_map) => custom_map,
+            None => {
+                panic!("Select custom level but custom_map is not set");
+            }
+        },
+        LevelChoice::None => panic!("Level Selection to None while going inside GamePlugin")
+    };
 
     commands.insert_resource(GameData {
     });
